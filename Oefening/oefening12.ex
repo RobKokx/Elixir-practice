a5062feded1250e94f53d18a29c8ae28d9ae4943
Pytugar <150233960+RobKokx@users.noreply.github.com>
2025-11-28 20:59:35 +0100

    stomme auto

--- Oefening/oefening12.ex
+++ Oefening/oefening12.ex
@@ -20,13 +20,54 @@ defmodule Persoon do
   end
 end
 
+defmodule Auto do
+  defstruct [:merk, :model, :jaar, kilometers: 0]
+
+  def nieuwe_auto(merk, model, jaar) do
+  %Auto{merk: merk, model: model, jaar: jaar}
+  end
+
+  def rijd(%Auto{} = auto, km) do
+    %{auto | kilometers: auto.kilometers + km}
+  end
+
+  def info(%Auto{} = auto) do
+    """
+    Ik rijd in een #{auto.merk},
+    Het is een #{auto.merk} #{auto.model}.
+    De auto is #{auto.jaar} geleden gebouwd.
+    Er is momenteel #{auto.kilometers} kilometer mee gereden.
+    """
+  end
+
+end
+
+
 # Maak structs
-rob = %Persoon{naam: "Rob", leeftijd: 28, stad: "Oss"}
-piet = Persoon.nieuw("Piet", 25)
+defmodule MakeStruct do
+  def make_struct(:persoon, name, leeftijd,email) do
+    %Persoon{naam: name, leeftijd: leeftijd, email: email}
+  end
+
+  def make_struct(:auto, merk, model, bouwjaar) do
+    %Auto{merk: merk, model: model, jaar: bouwjaar}
+  end
+end
+
+rob = MakeStruct.make_struct(:persoon, "Rob", 28, "r.roko@mail.test")
 
 IO.puts(Persoon.begroet(rob))
 IO.inspect(rob)
 
+
 # Verjaardag!
 rob_na_verjaardag = Persoon.verjaar(rob)
 IO.puts("Na verjaardag: #{rob_na_verjaardag.leeftijd}")
+
+mijn_auto = MakeStruct.make_struct(:auto, "toyota", "yaris", 21)
+IO.inspect(mijn_auto)
+na_rijden = Auto.rijd(mijn_auto, 241)
+IO.inspect(na_rijden)
+IO.puts(Auto.info(mijn_auto))
+mijn_auto = Auto.rijd(mijn_auto, 5421)
+IO.puts(Auto.info(mijn_auto))
